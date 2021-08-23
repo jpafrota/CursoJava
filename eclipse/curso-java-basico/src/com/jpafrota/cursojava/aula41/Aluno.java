b2002ea25597d4c2cb74f16fb35670708a7ddb6b
Jorge Frota <81825051+jpafrota@users.noreply.github.com>
2021-08-23 10:46:29 -0400

    Herança, polimorfismo e classes abstratas

---
+++ eclipse/curso-java-basico/src/com/jpafrota/cursojava/aula41/Aluno.java
@@ -0,0 +1,25 @@
+package com.jpafrota.cursojava.aula41;
+
+public class Aluno extends Pessoa {
+	
+	private String[] cursos;
+	private double[] notas;
+	
+	public String[] getCursos() {
+		return cursos;
+	}
+	public void setCursos(String[] cursos) {
+		this.cursos = cursos;
+	}
+	public double[] getNotas() {
+		return notas;
+	}
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+
+	public void testeAbstracao() {
+		System.out.println("aluno");
+	}
+	
+}
---
+++ eclipse/curso-java-basico/src/com/jpafrota/cursojava/aula43lab/Questao01.java
@@ -0,0 +1,10 @@
+package com.jpafrota.cursojava.aula43lab;
+
+public class Questao01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
