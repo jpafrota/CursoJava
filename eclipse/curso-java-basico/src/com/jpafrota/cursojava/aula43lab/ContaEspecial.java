12ff88cb9f7e11bbe89a0d4c913b2d84a3121635
jpafrota <81825051+jpafrota@users.noreply.github.com>
2021-08-23 19:22:41 -0400

    [Exercicio] Construção das classes e métodos do exercício 1 - aulas 37 a 43

---
+++ eclipse/curso-java-basico/src/com/jpafrota/cursojava/aula43lab/ContaEspecial.java
@@ -0,0 +1,64 @@
+package com.jpafrota.cursojava.aula43lab;
+
+public class ContaEspecial extends ContaBancaria {
+
+	private double limite;
+	private double limiteDisponivel;
+	
+	// Constructors
+	public ContaEspecial() { }
+	
+	public ContaEspecial(String nomeCliente, String numConta, double saldo, double limite) {
+		super(nomeCliente, numConta, saldo);
+		this.limite = limite;
+		this.limiteDisponivel = limite;
+		super.setTipo("Especial");
+	}
+	
+	// Getters and Setters
+	public double getLimite() {
+		return this.limite;
+	}
+	
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+	
+	// Methods
+	public boolean sacar(double saque) {
+		
+		if(saque <= this.getSaldo()) {
+			super.sacar(saque);
+			return true;
+		}
+		else if(saque <= Math.abs(this.getSaldo()) + limiteDisponivel){
+			double saldo = this.getSaldo();
+			saldo -= saque;
+			this.setSaldo(saldo);
+			atualizarLimite();
+		}
+		
+		return false;
+		
+	}
+	
+	public void depositar(double deposito) {
+		super.depositar(deposito);
+		atualizarLimite();
+	}
+	
+	private void atualizarLimite() {
+		
+		if(this.getSaldo() < 0) {
+			limiteDisponivel = limite + this.getSaldo();
+		}
+		
+	}
+	
+	public String toString() {
+		
+		return super.toString() + "Limite: " + this.limite + "\n";
+		
+	}
+	
+}
--- eclipse/curso-java-basico/src/com/jpafrota/cursojava/aula43lab/Questao01.java
+++ eclipse/curso-java-basico/src/com/jpafrota/cursojava/aula43lab/Questao01.java
@@ -3,8 +3,17 @@ package com.jpafrota.cursojava.aula43lab;
 public class Questao01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ContaEspecial conta1 = new ContaEspecial("Jorge", "466220", 1500, 500);
+		ContaPoupanca conta2 = new ContaPoupanca("Maycon", "713987", 200, 25);
+		ContaBancaria conta3 = new ContaBancaria("Serafina", "77828", 1200);
+		
+		conta1.depositar(1000);
+		
+		System.out.println(conta1.toString());
+		System.out.println(conta2.toString());
+		System.out.println(conta3.toString());
+		
 	}
 
 }
